271751244169a7936cd5a5204d2e2bc081d88543
andrsweb <proflinervk@gmail.com>
2023-08-04 07:54:51 +0700

    enf of project

---
+++ src/js/animationsOnScroll.js
@@ -0,0 +1,24 @@
+import { isInScope } from './common/global'
+
+document.addEventListener('scroll', () => {
+	const featured     = document.querySelector('.featured')
+	const testimonials = document.querySelector('.testimonials')
+	const help         = document.querySelector('.help')
+	const contact       = document.querySelector('.contact')
+
+	if (isInScope('.featured', window.scrollY)) {
+		featured.classList.add('scrolled')
+	}
+
+	if (isInScope('.testimonials', window.scrollY)) {
+		testimonials.classList.add('scrolled')
+	}
+
+	if (isInScope('.help', window.scrollY)) {
+		help.classList.add('scrolled')
+	}
+
+	if (isInScope('.contact', window.scrollY)) {
+		contact.classList.add('scrolled')
+	}
+})
--- src/js/common/global.js
+++ src/js/common/global.js
@@ -1,4 +1,18 @@
 let targetElement
 
 export const getTargetElement = () => targetElement
-export const setTargetElement = element => targetElement = element
+export const setTargetElement = element => targetElement = element  // Export functions for body lock
+
+let windowHeight = window.innerHeight                                 // Export isInScope function. If a block comes into range, you can perform some actions
+export const getWindowHeight = () => windowHeight
+export const isInScope = ( elementSelector, st, offset = 0 ) => {
+	const element  = document.querySelector( elementSelector )
+	if ( ! element) return
+	let bodyRect  = document.body.getBoundingClientRect(),
+		elemRect  = element.getBoundingClientRect(),
+		elemTop    = elemRect.top - bodyRect.top
+
+	if( ! element ) return
+
+	return st >= (elemTop - getWindowHeight() + offset) && st <= (elemTop + element.clientHeight - offset)
+}
